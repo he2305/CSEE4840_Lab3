55ff445e4c226b7dee6d87ff6fc7105d7584eebc
he2305 <hendo0218@gmail.com>
2025-04-04 18:48:57 -0400

    Update vga_ball.h

--- lab3-sw/vga_ball.h
+++ lab3-sw/vga_ball.h
@@ -4,23 +4,18 @@
 #include <linux/ioctl.h>
 
 typedef struct {
-	unsigned char red, green, blue;
-} vga_ball_color_t;
+  int x, y;
+} vga_ball_position_t;
   
 
 typedef struct {
-  vga_ball_color_t background;
-  char x;
-  char y;
+  vga_ball_position_t position;
 } vga_ball_arg_t;
 
 #define VGA_BALL_MAGIC 'q'
 
 /* ioctls and their arguments */
-#define VGA_BALL_WRITE_BACKGROUND _IOW(VGA_BALL_MAGIC, 1, vga_ball_arg_t *)
-#define VGA_BALL_READ_BACKGROUND  _IOR(VGA_BALL_MAGIC, 2, vga_ball_arg_t *)
-#define VGA_BALL_WRITE_COORDS _IOW(VGA_BALL_MAGIC, 3, vga_ball_arg_t *)
-#define H_SIZE 640 / 4
-#define V_SIZE 480 / 4
+#define VGA_BALL_WRITE_POSITION _IOW(VGA_BALL_MAGIC, 1, vga_ball_arg_t)
+#define VGA_BALL_READ_POSITION  _IOR(VGA_BALL_MAGIC, 2, vga_ball_arg_t)
 
 #endif
