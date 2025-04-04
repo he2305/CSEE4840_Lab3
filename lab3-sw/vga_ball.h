03c8f006f1d13c1516e0f840a4bce18482656224
he2305 <hendo0218@gmail.com>
2025-04-04 18:42:26 -0400

    Update vga_ball.h

--- lab3-sw/vga_ball.h
+++ lab3-sw/vga_ball.h
@@ -10,6 +10,8 @@ typedef struct {
 
 typedef struct {
   vga_ball_color_t background;
+  char x;
+  char y;
 } vga_ball_arg_t;
 
 #define VGA_BALL_MAGIC 'q'
@@ -17,5 +19,8 @@ typedef struct {
 /* ioctls and their arguments */
 #define VGA_BALL_WRITE_BACKGROUND _IOW(VGA_BALL_MAGIC, 1, vga_ball_arg_t *)
 #define VGA_BALL_READ_BACKGROUND  _IOR(VGA_BALL_MAGIC, 2, vga_ball_arg_t *)
+#define VGA_BALL_WRITE_COORDS _IOW(VGA_BALL_MAGIC, 3, vga_ball_arg_t *)
+#define H_SIZE 640 / 4
+#define V_SIZE 480 / 4
 
 #endif
