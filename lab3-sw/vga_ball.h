a79a307254db4c40892673fbb3e669867f1a4ad4
he2305 <hendo0218@gmail.com>
2025-04-04 18:28:19 -0400

    Update vga_ball.h

--- lab3-sw/vga_ball.h
+++ lab3-sw/vga_ball.h
@@ -4,7 +4,7 @@
 #include <linux/ioctl.h>
 
 typedef struct {
-  unsigned char red, green, blue;
+	unsigned char red, green, blue;
 } vga_ball_color_t;
   
 
@@ -15,7 +15,7 @@ typedef struct {
 #define VGA_BALL_MAGIC 'q'
 
 /* ioctls and their arguments */
-#define VGA_BALL_WRITE_BACKGROUND _IOW(VGA_BALL_MAGIC, 1, vga_ball_arg_t)
-#define VGA_BALL_READ_BACKGROUND  _IOR(VGA_BALL_MAGIC, 2, vga_ball_arg_t)
+#define VGA_BALL_WRITE_BACKGROUND _IOW(VGA_BALL_MAGIC, 1, vga_ball_arg_t *)
+#define VGA_BALL_READ_BACKGROUND  _IOR(VGA_BALL_MAGIC, 2, vga_ball_arg_t *)
 
 #endif
