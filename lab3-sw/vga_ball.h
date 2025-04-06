1c821ea0a5cc2a4bec608863cd8df46c2d0c7a84
he2305 <hendo0218@gmail.com>
2025-04-06 14:18:07 -0400

    Update vga_ball.h

--- lab3-sw/vga_ball.h
+++ lab3-sw/vga_ball.h
@@ -1,23 +1,28 @@
-#ifndef _VGA_Ball_H
+#ifndef _VGA_BALL_H
 #define _VGA_BALL_H
 
 #include <linux/ioctl.h>
 
-/* Directions the ball can go */
-#define HIGH_RIGHT 0
-#define LOW_RIGHT 1
-#define LOW_LEFT 2
-#define HIGH_LEFT 3
-
-/* Hard coded radius for right now */
-#define RADIUS 10
+typedef struct {
+  unsigned char red, green, blue;
+} vga_ball_color_t;
 
 typedef struct {
-  unsigned short x, y;   /* X and Y coordinates of the center of the ball */ 
+  unsigned short x;  // x corordinate (0-1279)
+  unsigned short y;  // y corordinate (0-479)
+} vga_ball_position_t;
+  
+typedef struct {
+  vga_ball_color_t background;
+  vga_ball_position_t position;
 } vga_ball_arg_t;
 
+#define VGA_BALL_MAGIC 'q'
+
 /* ioctls and their arguments */
-#define VGA_BALL_WRITE_COORD _IOW('q', 1, vga_ball_arg_t *)
-#define VGA_BALL_READ_COORD  _IOR('q', 2, vga_ball_arg_t *)
+#define VGA_BALL_WRITE_BACKGROUND _IOW(VGA_BALL_MAGIC, 1, vga_ball_arg_t)
+#define VGA_BALL_READ_BACKGROUND  _IOR(VGA_BALL_MAGIC, 2, vga_ball_arg_t)
+#define VGA_BALL_WRITE_POSITION   _IOW(VGA_BALL_MAGIC, 3, vga_ball_arg_t)
+#define VGA_BALL_READ_POSITION    _IOR(VGA_BALL_MAGIC, 4, vga_ball_arg_t)
 
 #endif
