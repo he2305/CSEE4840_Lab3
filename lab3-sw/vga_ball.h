11c94f0a01c2fba0255a9e9ce9367d5d07266f84
he2305 <hendo0218@gmail.com>
2025-04-06 14:00:37 -0400

    Update vga_ball.h

--- lab3-sw/vga_ball.h
+++ lab3-sw/vga_ball.h
@@ -1,45 +1,23 @@
-#ifndef _VGA_BALL_H
+#ifndef _VGA_Ball_H
 #define _VGA_BALL_H
 
 #include <linux/ioctl.h>
-#include <stddef.h>
+
+/* Directions the ball can go */
+#define HIGH_RIGHT 0
+#define LOW_RIGHT 1
+#define LOW_LEFT 2
+#define HIGH_LEFT 3
+
+/* Hard coded radius for right now */
+#define RADIUS 10
 
 typedef struct {
-    unsigned char red, green, blue;
-} vga_ball_color_t;
-
-typedef struct {
-    unsigned char left, top, right, bottom;
-} vga_ball_rect_t;
-
-typedef struct __attribute__((packed)) {
-  short x, y;
-  unsigned char radius;
-} vga_ball_circle_t;
-
-typedef struct {
-    short dx, dy;
-    short next_bound;
-    short corner;
-    float vx, vy, fx, fy;
-    float speed;
-} vga_ball_dir_t;
-
-typedef struct {
-    vga_ball_color_t c_color;
-    vga_ball_color_t bg_color;
-    vga_ball_circle_t circle;
+  unsigned short x, y;   /* X and Y coordinates of the center of the ball */ 
 } vga_ball_arg_t;
 
-#define VGA_BALL_MAGIC 'q'
-
-/* Ioctl to update colors (circle and background) */
-#define VGA_BALL_WRITE _IOW(VGA_BALL_MAGIC, 1, vga_ball_arg_t *)
-
-/* Ioctl to read current device state */
-#define VGA_BALL_READ  _IOR(VGA_BALL_MAGIC, 2, vga_ball_arg_t *)
-
-/* New: Ioctl to update just the circle coordinates */
-#define VGA_BALL_SET_CIRCLE _IOW(VGA_BALL_MAGIC, 3, vga_ball_circle_t *)
+/* ioctls and their arguments */
+#define VGA_BALL_WRITE_COORD _IOW('q', 1, vga_ball_arg_t *)
+#define VGA_BALL_READ_COORD  _IOR('q', 2, vga_ball_arg_t *)
 
 #endif
